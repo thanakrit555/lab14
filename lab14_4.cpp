7fcd0ee6b4a49c711762f392bb04672123209155
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-02-02 16:05:26 +0700

    lab14_4 completed

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -17,3 +17,54 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int N = 5;
+	for(int i = 0;i<N;i++){
+		int temp = rand()%12+1;
+		switch (temp)
+		{
+		case 1:
+			swap(a,b);
+			break;
+		case 2:
+			swap(a,c);
+			break;
+		case 3:
+			swap(a,d);
+			break;
+		case 4:
+			swap(b,a);
+			break;
+		case 5:
+			swap(b,c);
+			break;
+		case 6:
+			swap(b,d);
+			break;
+		case 7:
+			swap(c,a);
+			break;
+		case 8:
+			swap(c,b);
+			break;
+		case 9:
+			swap(c,d);
+			break;
+		case 10:
+			swap(d,a);
+			break;
+		case 11:
+			swap(d,b);
+			break;
+		case 12:
+			swap(d,c);
+			break;
+		}
+	}
+}
+void swap(int &x ,int &y){
+	int temp = x;
+	x = y;
+	y = temp;
+}
