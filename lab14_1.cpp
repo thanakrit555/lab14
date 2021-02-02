810e95101f5a881e6962531da9add09468043816
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-02-02 16:01:52 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,9 +1,23 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	void *y = &b;
+	void *z = &x;
 
-	
-	
+	cout <<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	cout <<(void *)&a<<" "<<(void *)&b<<" "<<(void *)&c<<" "<<(void *)&x<<" "<<(void *)&y<<" "<<(void *)&z<<endl; 
+	b = 'F';
+	cout <<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	b = 'W';
+	cout <<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	a=6;
+	cout <<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	a=7;
+	cout <<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
 	return 0;
 }
